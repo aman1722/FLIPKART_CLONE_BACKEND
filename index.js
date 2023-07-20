0b143e6677a64aaa240822e2c3ced4072ee134f0
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-07-20 17:04:45 +0530

    backend for deployement

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get("/",(req,res)=>{
+  res.status(200).send("Welcome To Flipkart Clone!")
+})
+
 app.use("/users",userRouter)
 app.use("/products",productRouter)
 
